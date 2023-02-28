917b16cae754151b5cdadd02e1352f3cae034de9
Nazik <nazikbaktygulova77@gmail.com>
2023-02-28 14:51:49 +0600

    Написан код для задачи №25 в папке hw_1

---
+++ hw_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 25: Напишите метод, который принимает на вход два числа A и B и возводит число А в натуральную степень В. 
+
+int Pow(int A, int B)
+{
+    int result = 1;
+    while (B != 0)
+    {
+        if ((B & 1) == 1)
+            result *= A;
+        A *= A;
+        B >>= 1;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Pow(A, B));
