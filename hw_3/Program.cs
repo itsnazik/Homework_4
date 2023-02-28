c7ecc73b8ca071afc875a4851e24f54d51a1c8b3
Nazik <nazikbaktygulova77@gmail.com>
2023-02-28 15:49:53 +0600

    Написан код для задачи №29 в папке hw_3

---
+++ hw_3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача №29: Напишите метод, котрый задает массив из N элементов и выводит их на экран
+
+void PrintArray()
+{
+    Console.WriteLine("Введите количество элементов: "); 
+    int N = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[N]; 
+    int i;  	
+   
+    for(i = 0; i < N; i++)  
+    {  
+	    Console.Write("{0}-элемент: ",i);
+	    array[i] = Convert.ToInt32(Console.ReadLine());  		
+    }  
+
+    Console.Write("Массив: ");  
+    for (i = 0; i < N; i++)
+        {
+            Console.Write(array[i] + ", ");
+        }
+}
+
+PrintArray();
