b6b546ebf1b58ed3380617d6e8de711345c3382a
Nazik <nazikbaktygulova77@gmail.com>
2023-02-28 15:03:35 +0600

    Написан код для задачи №27 в папке hw_2

---
+++ hw_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите метод, который принимает на вход число и выдает сумму цифр в числе
+ 
+static int Sum(int value)
+        {
+            int result = 0;
+            result += value % 10;
+            value /= 10;
+            if(value > 0)
+            {
+                result += Sum(value);
+            }
+            return result;
+        }
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Sum(number));
